f54a179601dd1ccfb89f981d05fd8f670b308b43
Joel Cogen <joel@joelcogen.com>
2010-10-31 14:53:03 +0100

    Fix to BaseRom

--- app/models/base_rom.rb
+++ app/models/base_rom.rb
@@ -1,7 +1,7 @@
 class BaseRom < ActiveRecord::Base
   belongs_to :device
   has_many :base_rom_packages
-  has_many :packages, :through => :base_rom_package
+  has_many :packages, :through => :base_rom_packages
 
   validates_presence_of :name
 end
