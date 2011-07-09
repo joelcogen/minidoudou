a41b0a460b196d06681ee3d6137a8b0ef9c86525
Joel Cogen <joel@joelcogen.com>
2011-07-09 21:08:55 +0200

    fixed rubygems problem on osx

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101129113611) do
+ActiveRecord::Schema.define(:version => 20110709154107) do
 
   create_table "apks", :force => true do |t|
     t.integer  "base_rom_id"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20101129113611) do
     t.boolean  "expert"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "deprecated"
   end
 
   create_table "base_rom_packages", :force => true do |t|
@@ -83,5 +84,19 @@ ActiveRecord::Schema.define(:version => 20101129113611) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                                 :default => "", :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.boolean  "admin"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
