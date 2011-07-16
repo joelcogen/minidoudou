823234f9f7c63f2907763e9d5536986b13e0d164
Joel Cogen <joel@joelcogen.com>
2011-07-16 19:11:32 +0200

    Fixed double upload issue

--- app/models/data_file.rb
+++ app/models/data_file.rb
@@ -1,3 +1,5 @@
+require 'ftools'
+
 class DataFile < ActiveRecord::Base
   def self.store(upload)
     return false if upload.nil?
@@ -10,11 +12,18 @@ class DataFile < ActiveRecord::Base
 
     # write the file
     if File.exist? path
-      false
-    else
-      File.open(path, "wb") { |f| f.write(upload['datafile'].read) }
+      num = 1
+      while File.exist? "#{path}_#{num.to_s}"
+        num += 1
+      end
+      path = "#{path}_#{num.to_s}"
+    end
+    
+    begin
+      File.cp upload['datafile'].path, path
       path
+    rescue
+      false
     end
   end
 end
-
